20d3e7e765fb466d8b9d21a9c4f0ed3f79bfb80b
dolly <dolli20@gmail.com>
2022-06-14 11:55:55 -0500

    GITtest

---
+++ src/main/java/SuccessTogather/NewProjectGIT/App.java
@@ -0,0 +1,13 @@
+package SuccessTogather.NewProjectGIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
